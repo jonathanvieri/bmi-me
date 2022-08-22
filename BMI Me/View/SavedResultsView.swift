6f04b9146d77772c3de9577b2dc550c06cabaf17
jonathanvieri <jonathanvieri1321@gmail.com>
2022-08-22 15:23:21 +0700

    Add description for SavedResults

--- BMI Me/View/SavedResultsView.swift
+++ BMI Me/View/SavedResultsView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct SavedResultsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            C.customColors.backgroundColor
+                .ignoresSafeArea(.all)
+            
+            VStack(spacing: 50) {
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250)
+                    .foregroundColor(C.customColors.buttonColor)
+                
+                Text("Feature is not available yet")
+                    .font(.title.bold())
+                    .foregroundColor(C.customColors.textColor)
+            }
+        }
     }
 }
 
